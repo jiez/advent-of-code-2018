c8a1d51101c66526b755839ea23ded6f101390c6
Jie Zhang <jzhang918@gmail.com>
2019-01-08 21:05:19 -0500

    don't hard code input path

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut freq = 0;
     let mut freqs = HashSet::new();
     let mut found = false;
-    let mut file = File::open("/home/jie/projects/rust/advent/1/input")
+    let mut file = File::open("input")
         .expect("Error cannot open the file");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
